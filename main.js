d288268f3343e4f90cbff443115f932e9c990144
Karol Filas <filaskarol@gmail.com>
2023-04-09 09:28:32 +0200

    finished adding to array app

--- main.js
+++ main.js
@@ -4,15 +4,18 @@ const div = document.querySelector('div')
 const addName = (e) => {
     e.preventDefault();
     const input = document.querySelector('input')
-    const name = input.value
+    const newName = input.value
 
-    if (name.length) {
-        for (iter of names) {
-
+    if (newName.length) {
+        for (name of names) {
+            if (name === newName) {
+                alert('to już znajduję się na liście')
+                return
+            }
         }
 
-        names.push(name)
-        div.textContent += name + ', '
+        names.push(newName)
+        div.textContent += newName + ', '
         input.value = ''
     }
 }
